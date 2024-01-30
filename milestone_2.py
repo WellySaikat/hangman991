6d3827762964b220fd12bf7118fbcfd869de5d04
Welly Saikat <wellysaikat@gmail.com>
2024-01-30 12:51:38 +0000

    adds milestone_2.py

---
+++ milestone_2.py
@@ -0,0 +1,14 @@
+#%%
+import random
+#  TODO create list of 5 favourite fruit
+
+word_list = ['banana', 'pineapple', 'papaya', 'dragonfruit', 'guava']
+print(word_list)
+
+word = random.choice(word_list)
+print(word)
+
+
+# %%
+
+guess = input('guess a letter')
