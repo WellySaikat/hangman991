2e9b0121b6eb368944d5c1d3c55050c2fda61d77
Welly Saikat <wellysaikat@gmail.com>
2024-01-31 11:14:26 +0000

    Updates milestone_4 with docstrings for added clarity

--- milestone_4.py
+++ milestone_4.py
@@ -4,6 +4,13 @@ import random
 
 class Hangman:
     def __init__(self, word_list, num_lives=5):
+        """
+        Initialize the Hangman game instance.
+        
+        Parameters:
+        word_list (list): A list of words to choose from for the game.
+        num_lives (int): The number of lives the player starts with.
+        """
         self.word_list = word_list
         self.num_lives = num_lives
         self.word = random.choice(self.word_list)
@@ -12,20 +19,36 @@ class Hangman:
         self.list_of_guesses = []
 
     def check_guess(self, guess):
+        """
+        Process the player's guess, updating game state and providing feedback.
+        
+        Parameters:
+        guess (str): The letter guessed by the player.
+        """
         guess = guess.lower()
         if guess in self.word:
+            for i in range(len(self.word)):
+                    if self.word[i] == guess:
+                        self.word_guessed[i] = guess
+            self.num_letters -= 1
             print(f"Good guess! {guess} is in the word.")
-    
+            print(self.num_letters)
         else :
             print(f"Sorry, {guess} is not in the word. Try again.")
+            self.num_lives -= 1
+            print(f"You have {self.num_lives} lives left.")
+
 
     def ask_for_input(self):
+        """
+        Continuously ask the player for a letter guess, checks whather the input is valid and different to previous guesses and adds the letter guess to the list of previous guesses.
+        """
         while True:
             guess = input('Guess a letter:')
 
     #check input is a single alphabetical character
 
-            if len(guess) != 1 or guess.isalpha() == False:
+            if len(guess) != 1 or not guess.isalpha():
                 print("Invalid letter. Please, enter a single alphabetical character.")
 
             elif guess in self.list_of_guesses:
